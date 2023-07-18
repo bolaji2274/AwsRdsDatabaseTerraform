f6fb3273b3958eec1fc7dd907f7c7aa6173e5611
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-18 18:26:12 +0100

    Initial commit'

---
+++ Database/variables.tf
@@ -0,0 +1,16 @@
+#variables.tf
+variable "access_key" {
+    description = "Access key to AWS console"
+}
+variable "secret_key" {
+    description = "Secret key to AWS console"
+}
+variable "region" {
+    description = "AWS region"
+}
+variable "username" {
+    description = "This set a username to the databases"
+}
+variable "password" {
+    description = "This set a password to the databases"
+}
